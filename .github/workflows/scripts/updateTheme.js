57082a2995adf8ea9067a35643f46a6e7332c46b
Matty Williams <mattyqwilliams@gmail.com>
2023-05-24 11:09:30 -0700

    Update updateTheme.js

--- .github/workflows/scripts/updateTheme.js
+++ .github/workflows/scripts/updateTheme.js
@@ -19,6 +19,8 @@ if (!themeId || !filePath) {
   process.exit(1);
 }
 
+const MAX_WAIT_TIME = 5 * 60 * 1000;
+
 async function updateTheme(themeId, replaceSettings) {
   try {
     const response = await instance.post(`/guide/theming/jobs/themes/updates`, {
@@ -30,13 +32,27 @@ async function updateTheme(themeId, replaceSettings) {
         }
       }
     });
+    console.log('::group::Update Theme Response');
+    const prettyResponse = JSON.stringify(response.data, null, 2);
+    console.log(prettyResponse);
+    console.log('::endgroup::');
+
+    fs.appendFileSync(process.env.GITHUB_STEP_SUMMARY, `\n\n## Update Theme Response\n\`\`\`json\n${prettyResponse}\n\`\`\``);
+
     return {
       jobId: response.data.job.id,
       uploadUrl: response.data.job.data.upload.url,
       uploadParameters: response.data.job.data.upload.parameters
     };
   } catch (error) {
-    console.error('Error starting theme update:', JSON.stringify(error, null, 2));
+    console.log('::group::Action failed with error');
+    const prettyError = JSON.stringify(error, null, 2);
+    console.log(prettyError);
+    console.log('::endgroup::');
+
+    fs.appendFileSync(process.env.GITHUB_STEP_SUMMARY, `\n\n## Update Theme Error\n\`\`\`json\n${prettyError}\n\`\`\``);
+
+    process.exit(1);
   }
 }
 
@@ -58,19 +74,44 @@ async function uploadThemeFile(uploadUrl, uploadParameters, filePath) {
 
   try {
     const response = await axios.post(uploadUrl, form, { headers: form.getHeaders() });
-    console.log('Upload response:', response.data);
+    console.log('::group::Upload Theme File Response');
+    const prettyResponse = JSON.stringify(response.data, null, 2);
+    console.log(prettyResponse);
+    console.log('::endgroup::');
+
+    fs.appendFileSync(process.env.GITHUB_STEP_SUMMARY, `\n\n## Upload Theme File Response\n\`\`\`json\n${prettyResponse}\n\`\`\``);
   } catch (error) {
-    console.error('Error uploading theme file:', JSON.stringify(error, null, 2));
+    console.log('::group::Action failed with error');
+    const prettyError = JSON.stringify(error, null, 2);
+    console.log(prettyError);
+    console.log('::endgroup::');
+
+    fs.appendFileSync(process.env.GITHUB_STEP_SUMMARY, `\n\n## Upload Theme File Error\n\`\`\`json\n${prettyError}\n\`\`\``);
+
+    process.exit(1);
   }
 }
 
 async function checkUpdateJobStatus(jobId) {
   try {
     const response = await instance.get(`/guide/theming/jobs/${jobId}`);
-    console.log('Job Status Response:', JSON.stringify(response.data, null, 2));
+    if (response.data.job.status !== 'pending') {
+      console.log('::group::Check Update Job Status Response');
+      const prettyResponse = JSON.stringify(response.data, null, 2);
+      console.log(prettyResponse);
+      console.log('::endgroup::');
+      fs.appendFileSync(process.env.GITHUB_STEP_SUMMARY, `\n\n## Check Update Job Status Response\n\`\`\`json\n${prettyResponse}\n\`\`\``);
+    }
     return response.data.job;
   } catch (error) {
-    console.error('Error checking job status:', JSON.stringify(error, null, 2));
+    console.log('::group::Action failed with error');
+    const prettyError = JSON.stringify(error, null, 2);
+    console.log(prettyError);
+    console.log('::endgroup::');
+
+    fs.appendFileSync(process.env.GITHUB_STEP_SUMMARY, `\n\n## Check Update Job Status Error\n\`\`\`json\n${prettyError}\n\`\`\``);
+
+    process.exit(1);
   }
 }
 
@@ -83,18 +124,28 @@ async function run() {
   console.log('Theme file uploaded.');
 
   let jobStatus = await checkUpdateJobStatus(jobId);
+  const startTime = Date.now();
   while (jobStatus.status !== 'completed' && jobStatus.status !== 'failed') {
+    if (Date.now() - startTime > MAX_WAIT_TIME) {
+      console.error('Job status check timed out');
+      fs.appendFileSync(process.env.GITHUB_STEP_SUMMARY, `\n\n## Job Status Check Timeout\nJob did not complete within ${MAX_WAIT_TIME / 1000} seconds`);
+      process.exit(1);
+    }
     console.log('Waiting for job to complete...');
     await new Promise(resolve => setTimeout(resolve, 5000));
     jobStatus = await checkUpdateJobStatus(jobId);
   }
 
   if (jobStatus.status === 'failed') {
-    console.log('Job failed:', JSON.stringify(jobStatus.errors, null, 2));
+    console.error('Job failed:', JSON.stringify(jobStatus.errors, null, 2));
+    fs.appendFileSync(process.env.GITHUB_STEP_SUMMARY, `\n\n## Job Failed\n\`\`\`json\n${JSON.stringify(jobStatus.errors, null, 2)}\n\`\`\``);
+    process.exit(1);
   } else if (jobStatus.status === 'completed') {
     console.log('Job completed. Theme updated.');
   } else {
-    console.log('Job in unexpected state:', jobStatus.status);
+    console.error('Job in unexpected state:', jobStatus.status);
+    fs.appendFileSync(process.env.GITHUB_STEP_SUMMARY, `\n\n## Unexpected Job State\n\`\`\`json\n${jobStatus.status}\n\`\`\``);
+    process.exit(1);
   }
 }
 

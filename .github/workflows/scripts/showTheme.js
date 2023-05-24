41c1d010da01198d3666699b439ba4474a7fe205
Matty Williams <mattyqwilliams@gmail.com>
2023-05-24 11:09:18 -0700

    Update showTheme.js

--- .github/workflows/scripts/showTheme.js
+++ .github/workflows/scripts/showTheme.js
@@ -1,4 +1,5 @@
 const axios = require('axios');
+const fs = require('fs');
 
 const instance = axios.create({
   baseURL: `https://${process.env['ZENDESK_SUBDOMAIN']}.zendesk.com/api/v2`,
@@ -17,10 +18,20 @@ if (!themeId) {
 
 instance.get(`/guide/theming/themes/${themeId}`)
   .then((response) => {
-    console.log(JSON.stringify(response.data));
-    return JSON.stringify(response.data);
+    console.log('::group::Theme Response');
+    const prettyResponse = JSON.stringify(response.data, null, 2);
+    console.log(prettyResponse);
+    console.log('::endgroup::');
+
+    fs.appendFileSync(process.env.GITHUB_STEP_SUMMARY, `\n\n## Show Theme Response\n\`\`\`json\n${prettyResponse}\n\`\`\``);
   })
   .catch((error) => {
-    console.log(JSON.stringify(error));
-    return new Error(JSON.stringify(error));
+    console.log('::group::Action failed with error');
+    const prettyError = JSON.stringify(error, null, 2);
+    console.log(prettyError);
+    console.log('::endgroup::');
+
+    fs.appendFileSync(process.env.GITHUB_STEP_SUMMARY, `\n\n## Show Theme Error\n\`\`\`json\n${prettyError}\n\`\`\``);
+
+    process.exit(1);
   });
